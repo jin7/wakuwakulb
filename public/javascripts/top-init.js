73c488f2e2405ef589b0f811c28aad0da9be4f36
Keisuke Kuramoto <zoupon@gmail.com>
2013-09-16 17:55:43 +0900

    modify top-screen
    アニメーション追加
    ホールイメージに変更

---
+++ public/javascripts/top-init.js
@@ -0,0 +1,6 @@
+window.addEventListener( 'DOMContentLoaded', function () {
+    document.querySelector('img').addEventListener('click', function() {
+      initAll("/performance");
+    });
+    connectTop();
+  }, false);
